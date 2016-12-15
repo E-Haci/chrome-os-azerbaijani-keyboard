ba42d689e72e221a6c2145f98829fbf64a064d17
Elvin Haci <aktivhelp@yandex.ru>
2016-12-15 11:10:52 +0400

    added files via upload

---
+++ background.js
@@ -0,0 +1,92 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+var AltGr = { PLAIN: "plain", ALTERNATE: "alternate" };
+var Shift = { PLAIN: "plain", SHIFTED: "shifted" };
+
+var contextID = -1;
+var altGrState = AltGr.PLAIN;
+var shiftState = Shift.PLAIN;
+var lastRemappedKeyEvent = undefined;
+
+var lut = {
+"KeyW": { "plain": {"plain": "ü", "shifted": "Ü"}, "alternate": {"plain": "w", "shifted":"W"}, "code": "KeyW"},
+"BracketLeft": { "plain": {"plain": "ö", "shifted": "Ö"}, "alternate": {"plain": "[", "shifted":"{"}, "code": "BracketLeft"},
+"BracketRight": { "plain": {"plain": "ğ", "shifted": "Ğ"}, "alternate": {"plain": "]", "shifted":"}"}, "code": "BracketRight"},
+"Semicolon": { "plain": {"plain": "ı", "shifted": "I"}, "alternate": {"plain": "", "shifted":""}, "code": "Semicolon"},
+"Quote": { "plain": {"plain": "ə", "shifted": "Ə"}, "alternate": {"plain": "", "shifted":""}, "code": "Quote"},
+"Comma": { "plain": {"plain": "ç", "shifted": "Ç"}, "alternate": {"plain": "", "shifted":""}, "code": "Comma"},
+"Period": { "plain": {"plain": "ş", "shifted": "Ş"}, "alternate": {"plain": "", "shifted":""}, "code": "Period"},
+"Slash": { "plain": {"plain": ".", "shifted": ","}, "alternate": {"plain": "", "shifted":""}, "code": "Slash"},
+};
+    
+
+chrome.input.ime.onFocus.addListener(function(context) {
+  contextID = context.contextID;
+});
+
+function updateAltGrState(keyData) {
+  altGrState = (keyData.code == "AltRight") ? ((keyData.type == "keydown") ? AltGr.ALTERNATE : AltGr.PLAIN)
+                                              : altGrState;
+}
+
+function updateShiftState(keyData) {
+  shiftState = ((keyData.shiftKey && !(keyData.capsLock)) || (!(keyData.shiftKey) && keyData.capsLock)) ? 
+                 Shift.SHIFTED : Shift.PLAIN;
+}
+
+function isPureModifier(keyData) {
+  return (keyData.key == "Shift") || (keyData.key == "Ctrl") || (keyData.key == "Alt");
+}
+
+function isRemappedEvent(keyData) {
+  // hack, should check for a sender ID (to be added to KeyData)
+  return lastRemappedKeyEvent != undefined &&
+         (lastRemappedKeyEvent.key == keyData.key &&
+          lastRemappedKeyEvent.code == keyData.code &&
+          lastRemappedKeyEvent.type == keyData.type
+         ); // requestID would be different so we are not checking for it  
+}
+
+
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+      var handled = false;
+      
+      if (isRemappedEvent(keyData)) {
+        lastRemappedKeyEvent = undefined;
+        return handled;
+      }
+
+      updateAltGrState(keyData);
+      updateShiftState(keyData);
+                
+      if (lut[keyData.code]) {
+          var remappedKeyData = keyData;
+          remappedKeyData.key = lut[keyData.code][altGrState][shiftState];
+          remappedKeyData.code = lut[keyData.code].code;
+        
+        if (chrome.input.ime.sendKeyEvents != undefined) {
+          chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [remappedKeyData]});
+          handled = true;
+          lastRemappedKeyEvent = remappedKeyData;
+        } else if (keyData.type == "keydown" && !isPureModifier(keyData)) {
+          chrome.input.ime.commitText({"contextID": contextID, "text": remappedKeyData.key});
+          handled = true;
+        }
+      }
+      
+      return handled;
+});
